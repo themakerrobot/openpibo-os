b848a7b5af43477fc9d7b68a4709fc7157332354
leeyunjai82 <leeyunjai@circul.us>
2022-02-10 18:47:34 +0900

    update

--- project2/lib.py
+++ project2/lib.py
@@ -1,275 +1,293 @@
-
-import os,json,shutil
-import argparse
-
-from openpibo.speech import Dialog
-from openpibo.vision import *
+from openpibo.vision import Camera
+from openpibo.vision import Face
+from openpibo.vision import Detect
 from openpibo.device import Device
-from openpibo.motion import Motion
 from openpibo.oled import Oled
+from openpibo.speech import Dialog
+from openpibo.motion import Motion
 
+import time, datetime
 import base64
 import cv2
-import asyncio
-from threading import Thread, Lock
 from queue import Queue
-import datetime
+from threading import Thread, Lock
 
 def to_base64(im):
   im = cv2.imencode('.jpg', im)[1].tobytes()
   return base64.b64encode(im).decode('utf-8')
 
 class Pibo:
-  def __init__(self, emit_function=None):
-    self.emit = emit_function
-
-  def start(self):
-    self.chat_list = []
-    self.system_value = ['','','','','','']
-    self.neopixel_value = [0,0,0,0,0,0]
-    self.next_cmd = [False, ""]
-    self.que = Queue()
-
-    self.__d = [0, 0, -80, 0, 0, 0, 0, 0, 80, 0] # current d value
-    self.__p = [] # current pos list value
-    self.__j = {} # current json value
+    def __init__(self, emit_func=None):
+        self.emit_func = emit_func
+        self.onoff = False
     
-    cam = Camera()
-    fac = Face()
-    det = Detect()
-    mot = Motion()
-    dev = Device()
-    ole = Oled()
-    dialog = Dialog()
-
-    mot.set_speeds([50,50,50,50,50, 50,50,50,50,50])
-    mot.set_accelerations([0,0,0,0,0,0,0,0,0,0])
-    mot.set_motors(__d)
-
-    send_message(Device.code_list['BATTERY'], "on")
-    send_message(Device.code_list['PIR'], "on")
-    send_message(Device.code_list['DC_CONN'])
-    send_message(Device.code_list['NEOPIXEL_EACH'], ",".join([str(_) for _ in neopixel_value]))
-
-    Thread(name="device_loop", target=device_loop, args=(), daemon=True).start()
-    Thread(name="vision_loop", target=vision_loop, args=(), daemon=True).start()
-    Thread(name="system_loop", target=system_loop, args=(), daemon=True).start()
-
-    try:
-      with open("/home/pi/mymotion.json", "rb") as f:
-        __j = json.load(f)
-    except Exception as ex:
-      print("Error:", ex)
-      pass
-
-    socketio.emit('disp_code', __j)
-    self.onoff = True
-
-  def stop(self):
-    self.onoff = False
-
-  def system_loop(self):
-    while True:
-      if self.onoff == False:
-        break
-      res = os.popen('/home/pi/openpibo-tools/project/system.sh').read().split(',')
-      socketio.emit("system", res)
-      time.sleep(10)
-
-  # vision
-  def vision_loop():
-    while True:
-      if self.onoff == False:
-        break
-      self.frame = cam.read()  # read the camera frame
-      im = self.frame.copy()
-      socketio.emit('stream', to_base64(cv2.resize(im, (320,240))), callback=None)
-  
-  def cartoon(self):
-    im = self.frame.copy()
-    im = cam.cartoonize(im)
-    socketio.emit('cartoon', to_base64(im), callback=None)
-
-  def detect(self):
-    im = self.frame.copy()
-    faces = fac.detect(im)
-    objs = det.detect_object(im)
-    qr = det.detect_qr(im)
-
-    res_face = ""
-    res_qr = ""
-    res_object = ""
-
-    if len(faces) > 0:
-      x,y,w,h = faces[0]
-      face = fac.get_ageGender(im, faces[0])
-      colors = (200,100,0) if face["gender"] == "Male" else (100,200,0)
-      cam.rectangle(im, (x,y), (x+w, y+h), colors, 1)
-      cam.putText(im, face["gender"]+face["age"], (x-10, y-10),0.6,colors,2)
-      res_face += "[{}/{}-({},{})] ".format(face["gender"], face["age"], x, y)
-
-    if qr["type"] != "":
-      x1,y1,x2,y2 = qr["position"]
-      colors = (100,0,200)
-      cam.rectangle(im, (x1,y1), (x2, y2),colors,1)
-      cam.putText(im, "QR", (x1-10, y1-10),0.6,colors,2)
-      res_qr += "[{}-({},{})] ".format(qr["data"], x1, y1)
-
-    for obj in objs:
-      x1,y1,x2,y2 = obj["position"]
-      colors = (100,100,200)
-      cam.rectangle(im, (x1,y1), (x2, y2),colors,1)
-      cam.putText(im, obj["name"], (x1-10, y1-10),0.6,colors,2)
-      res_object += "[{}-({},{})] ".format(obj["name"], x1, y1)
-
-    socketio.emit('detect', {"img":to_base64(im), "data":{"face":res_face, "qr":res_qr, "object":res_object}}, callback=None)
-
-
-  ## chatbot
-  def question(self, q):
-    ans = dialog.get_dialog(q)
-    self.chat_list.append([str(datetime.datetime.now()).split('.')[0], q, ans])
-    socketio.emit('answer', {"answer":ans, "chat_list":list(reversed(self.chat_list))})
-
-    if len(self.chat_list) == 5:
-      self.chat_list.pop(0)
-
-
-
-  ## device
-  def send_message(self, code, data=""):
-    self.que.put("#{}:{}!".format(code, data))
-
-  def set_neopixel(self, d):
-    self.neopixel_value[d['idx']] = d['value']
-    self.send_message(Device.code_list['NEOPIXEL_EACH'], ",".join([str(_) for _ in self.neopixel_value]))
-
-  def set_oled(self, d):
-    ole.clear()
-    ole.set_font(size=d['size'])
-    ole.draw_text((d['x'], d['y']), d['text'])
-    ole.show()
-
-
-  def decode_pkt(self, pkt):
-    print("Recv:", pkt, pkt.split(":")[1].split("-"))
-    pkt = pkt.split(":")
-    code, data = pkt[0], pkt[1]
-
-    if code == "15": # battery
-      socketio.emit('update_battery', data, callback=None)
-
-    if code == "14": # dc
-      self.system_value[2] = data
-      socketio.emit('update_device', self.system_value, callback=None)
-
-    if code == "40": # system
-      data = data.split("-")
-
-      if data[2] == '':
-        data[2] = self.system_value[2]
-
-      self.system_value = data
-      socketio.emit('update_device', self.system_value, callback=None)
-
-  def device_loop(self):
-    system_check_time = time.time()
-    battery_check_time = time.time()
-
-    while True:
-      if self.onoff == False:
-        break
+    def emit(self, key, data, callback=None):
+        if self.emit_func == None:
+            print("No emit_func")
+        else:
+            self.emit_func(key, data)
         
-      if que.qsize() > 0:
-        data = dev.send_raw(que.get())
-        decode_pkt(data)
+    ## vision
+    def vision_start(self):
+        self.cam = Camera()
+        self.fac = Face()
+        self.det = Detect()
+        self.vision_flag = True
+        Thread(name="vision_loop", target=self.vision_loop, args=(), daemon=True).start()
 
-      if time.time() - system_check_time > 1:  # 시스템 메시지 1초 간격 전송
-        data = dev.send_cmd(Device.code_list['SYSTEM'])
-        decode_pkt(data)
+    def vision_stop(self):
+        self.vision_flag = False
+        del self.cam, self.fac, self.det
+
+    def vision_loop(self):
+        while True:
+            if self.vision_flag == False:
+                break
+            self.frame = self.cam.read()  # read the camera frame
+            self.emit('stream', to_base64(cv2.resize(self.frame, (320,240))), callback=None)
+
+    def cartoon(self):
+        im = self.frame.copy()
+        self.emit('cartoon', to_base64(self.cam.cartoonize(im)), callback=None)
+
+    def object_detect(self):
+        im = self.frame.copy()
+        faces = self.fac.detect(im)
+        objs = self.det.detect_object(im)
+        qr = self.det.detect_qr(im)
+
+        res_face = ""
+        res_qr = ""
+        res_object = ""
+
+        if len(faces) > 0:
+            x,y,w,h = faces[0]
+            face = self.fac.get_ageGender(im, faces[0])
+            colors = (200,100,0) if face["gender"] == "Male" else (100,200,0)
+            self.cam.rectangle(im, (x,y), (x+w, y+h), colors, 1)
+            self.cam.putText(im, face["gender"]+face["age"], (x-10, y-10),0.6,colors,2)
+            res_face += "[{}/{}-({},{})] ".format(face["gender"], face["age"], x, y)
+
+        if qr["type"] != "":
+            x1,y1,x2,y2 = qr["position"]
+            colors = (100,0,200)
+            self.cam.rectangle(im, (x1,y1), (x2, y2),colors,1)
+            self.cam.putText(im, "QR", (x1-10, y1-10),0.6,colors,2)
+            res_qr += "[{}-({},{})] ".format(qr["data"], x1, y1)
+
+        for obj in objs:
+            x1,y1,x2,y2 = obj["position"]
+            colors = (100,100,200)
+            self.cam.rectangle(im, (x1,y1), (x2, y2),colors,1)
+            self.cam.putText(im, obj["name"], (x1-10, y1-10),0.6,colors,2)
+            res_object += "[{}-({},{})] ".format(obj["name"], x1, y1)
+
+        self.emit('detect', {"img":to_base64(im), "data":{"face":res_face, "qr":res_qr, "object":res_object}}, callback=None)
+
+    ## device
+    def device_start(self):
+        self.system_value = ['','','','','','']
+        self.neopixel_value = [0,0,0,0,0,0]
+        self.devque = Queue()
+        self.device_flag = True
+        self.dev = Device()
+        self.ole = Oled()
+        Thread(name="device_loop", target=self.device_loop, args=(), daemon=True).start()
+
+        self.send_message(Device.code_list['BATTERY'], "on")
+        self.send_message(Device.code_list['PIR'], "on")
+        self.send_message(Device.code_list['DC_CONN'])
+        self.send_message(Device.code_list['NEOPIXEL_EACH'], ",".join([str(_) for _ in self.neopixel_value]))
+
+    def device_stop(self):
+        self.device_flag = False
+        del self.dev, self.ole
+
+    def send_message(self, code, data=""):
+        self.devque.put("#{}:{}!".format(code, data))
+
+    def decode_pkt(self, pkt):
+        print("Recv:", pkt, pkt.split(":")[1].split("-"))
+        pkt = pkt.split(":")
+        code, data = pkt[0], pkt[1]
+
+        if code == "15": # battery
+            self.emit('update_battery', data, callback=None)
+
+        if code == "14": # dc
+            self.system_value[2] = data
+            self.emit('update_device', self.system_value, callback=None)
+
+        if code == "40": # system
+            data = data.split("-")
+
+            if data[2] == '':
+                data[2] = self.system_value[2]
+
+            self.system_value = data
+            self.emit('update_device', self.system_value, callback=None)
+
+    def device_loop(self):
         system_check_time = time.time()
-
-      if time.time() - battery_check_time > 10: # 배터리 메시지 10초 간격 전송
-        data = dev.send_cmd(Device.code_list['BATTERY'])
-        decode_pkt(data)
         battery_check_time = time.time()
 
-      time.sleep(0.01)
+        while True:
+            if self.device_flag == False:
+                break
 
-## motion
+            if self.devque.qsize() > 0:
+                data = self.dev.send_raw(self.devque.get())
+                self.decode_pkt(data)
 
-def make_raw(self):
-  return {'init_def':1, 'init':__p[0]['d'], 'pos':__p[1:]} if __p[0]['seq'] == 0 else {'init_def':0, 'pos':__p}
+            if time.time() - system_check_time > 1:  # 시스템 메시지 1초 간격 전송
+                data = self.dev.send_cmd(Device.code_list['SYSTEM'])
+                self.decode_pkt(data)
+                system_check_time = time.time()
 
-def motor_init(self):
-  socketio.emit('init_motion', __d)
-  socketio.emit('disp_record', __p)
+            if time.time() - battery_check_time > 10: # 배터리 메시지 10초 간격 전송
+                data = self.dev.send_cmd(Device.code_list['BATTERY'])
+                self.decode_pkt(data)
+                battery_check_time = time.time()
 
-def set_pos(self, motor_idx, motor_val):
-  __d[motor_idx] = motor_val
-  mot.set_speed(motor_idx, 50)
-  mot.set_acceleration(motor_idx, 0)
-  mot.set_motor(motor_idx, motor_val)
+                time.sleep(0.01)
 
-def add_frame(self, seq):
-  seq = int(seq)
-  _check = False
-  for idx, pos in enumerate(__p):
-    if pos['seq'] == seq:
-      __p[idx] = {"d": __d[:], "seq": int(seq)}
-      _check = True
-      break
+    def set_neopixel(self, d):
+        self.neopixel_value[d['idx']] = d['value']
+        self.send_message(Device.code_list['NEOPIXEL_EACH'], ",".join([str(_) for _ in self.neopixel_value]))
 
-  if _check == False:
-    __p.append({"d": __d[:], "seq": int(seq)})
-    __p.sort(key=lambda x: x['seq'])
+    def set_oled(self, d):
+        self.ole.clear()
+        self.ole.set_font(size=d['size'])
+        self.ole.draw_text((d['x'], d['y']), d['text'])
+        self.ole.show()
 
-  socketio.emit('disp_record', __p)
+    ## chatbot
+    def chatbot_start(self):
+        self.chat_list = []
+        self.dialog = Dialog()
 
-def remove_frame(self, seq):
-  for idx, pos in enumerate(__p):
-    if pos['seq'] == seq:
-      del __p[idx]
-      break
+    def chatbot_stop(self):
+        self.chat_list = []
+        del self.dialog
 
-def init_frame(self):
-  __p = []
+    def question(self, q):
+        ans = self.dialog.get_dialog(q)
+        self.chat_list.append([str(datetime.datetime.now()).split('.')[0], q, ans])
+        self.emit('answer', {"answer":ans, "chat_list":list(reversed(self.chat_list))})
 
-def set_motion(self, cycle):
-  raw = make_raw()
-  mot.set_motion_raw(raw, int(cycle))
+        if len(self.chat_list) == 5:
+            self.chat_list.pop(0)
 
-def add_motion(self, name):
-  __j[name] = make_raw()
-  socketio.emit('disp_code', __j)
+    ## motion
+    def motion_start(self):
+        self.__d = [0, 0, -80, 0, 0, 0, 0, 0, 80, 0] # current d value
+        self.__p = [] # current pos list value
+        self.__j = {} # current json value
+        self.mot = Motion()
+        self.mot.set_speeds([50,50,50,50,50, 50,50,50,50,50])
+        self.mot.set_accelerations([0,0,0,0,0,0,0,0,0,0])
+        self.mot.set_motors(self.__d)
 
-def load_motion(self, name):
-  if name in __j:
-    __p = []
-    a = __j[name]
+        try:
+            with open("/home/pi/mymotion.json", "rb") as f:
+                self.__j = json.load(f)
+                self.emit('disp_code', self.__j)
+        except Exception as ex:
+            print("Error:", ex)
+            pass
 
-    if 'init_def' in a and 'init' in a:
-      __p.append({'d':a['init'], 'seq':0})
-    if 'pos' in a:
-      for item in a['pos']:
-        __p.append(item)
-  socketio.emit('disp_record', __p)
+    def motion_stop(self):
+        self.__d = [0, 0, -80, 0, 0, 0, 0, 0, 80, 0] # current d value
+        self.__p = [] # current pos list value
+        self.__j = {} # current json value
+        del self.mot
 
-def del_motion(self, name):
-  if name in __j:
-    del __j[name]
-  socketio.emit('disp_code', __j)
+    def make_raw(self):
+        return {'init_def':1, 'init':self.__p[0]['d'], 'pos':self.__p[1:]} if self.__p[0]['seq'] == 0 else {'init_def':0, 'pos':self.__p}
 
-def export(self):
-  with open("/home/pi/mymotion.json", "w") as f:
-    json.dump(__j, f)
-  shutil.chown('/home/pi/mymotion.json', 'pi', 'pi')
-  socketio.emit('disp_code', __j)
+    def motor_init(self):
+        self.emit('init_motion', self.__d)
+        self.emit('disp_record', self.__p)
 
-def display(self):
-  socketio.emit('disp_code', __j)
+    def set_pos(self, idx, pos):
+        self.__d[idx] = pos
+        self.mot.set_speed(idx, 50)
+        self.mot.set_acceleration(idx, 0)
+        self.mot.set_motor(idx, pos)
 
-def reset(self):
-  __j = {}
-  os.remove('/home/pi/mymotion.json')
-  socketio.emit('disp_code', __j)
+    def add_frame(self, seq):
+        seq = int(seq)
+        _check = False
+        for idx, pos in enumerate(self.__p):
+            if pos['seq'] == seq:
+                self.__p[idx] = {"d": self.__d[:], "seq": int(seq)}
+                _check = True
+                break
+
+        if _check == False:
+            self.__p.append({"d": self.__d[:], "seq": int(seq)})
+            self.__p.sort(key=lambda x: x['seq'])
+
+        self.emit('disp_record', self.__p)
+
+    def remove_frame(self, seq):
+        for idx, pos in enumerate(self.__p):
+            if pos['seq'] == seq:
+                del self.__p[idx]
+                break
+
+    def init_frame(self):
+        self.__p = []
+
+    def play_frame(self, cycle):
+        raw = self.make_raw()
+        self.mot.set_motion_raw(raw, int(cycle))
+
+    def add_motion(self, name):
+        self.__j[name] = self.make_raw()
+        self.emit('disp_code', self.__j)
+
+    def load_motion(self, name):
+        if name in self.__j:
+            self.__p = []
+            a = self.__j[name]
+
+            if 'init_def' in a and 'init' in a:
+                self.__p.append({'d':a['init'], 'seq':0})
+            if 'pos' in a:
+                for item in a['pos']:
+                    self.__p.append(item)
+        self.emit('disp_record', self.__p)
+
+    def del_motion(self, name):
+        if name in self.__j:
+            del self.__j[name]
+        self.emit('disp_code', self.__j)
+
+    def save(self):
+        with open("/home/pi/mymotion.json", "w") as f:
+            json.dump(self.__j, f)
+        shutil.chown('/home/pi/mymotion.json', 'pi', 'pi')
+        self.emit('disp_code', self.__j)
+
+    def display(self):
+        self.emit('disp_code', self.__j)
+
+    def reset(self):
+        self.__j = {}
+        os.remove('/home/pi/mymotion.json')
+        self.emit('disp_code', self.__j)
+
+    def start(self):
+        self.vision_start()
+        self.device_start()
+        self.chatbot_start()
+        self.motion_start()
+        self.onoff = True
+
+    def stop(self):
+        self.vision_stop()
+        self.device_stop()
+        self.chatbot_stop()
+        self.motion_stop()
+        self.onoff = False
--- project2/main.py
+++ project2/main.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import os,json,shutil
 import argparse
 
 from lib import Pibo
 
-import base64
-import cv2
-import asyncio
-from threading import Thread, Lock
-from queue import Queue
-import datetime
+from threading import Thread
+import time, datetime, os
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -22,57 +17,109 @@ def main():
 # vision
 @socketio.on('cartoon')
 def f_cartoon(d, method=['GET', 'POST']):
+  if pibo.onoff:
+    pibo.cartoon()
 
 @socketio.on('detect')
 def f_detect(d, method=['GET', 'POST']):
+  if pibo.onoff:
+    pibo.object_detect()
 
+# device
 @socketio.on('set_neopixel')
 def f_set_neopixel(d, methods=['GET', 'POST']):
+  if pibo.onoff:
+    pibo.set_neopixel(d)
 
 @socketio.on('set_oled')
 def f_set_oled(d, method=['GET', 'POST']):
+  if pibo.onoff:
+    pibo.set_oled(d)
 
+# chatbot
 @socketio.on('question')
 def question(q):
+  if pibo.onoff:
+    pibo.question(q)
 
+# motion
 @socketio.on('motor_init')
 def motor_init():
+  if pibo.onoff:
+    pibo.motion_init()
 
 @socketio.on('set_pos')
-def set_pos(motor_idx, motor_val):
+def set_pos(idx, pos):
+  if pibo.onoff:
+    pibo.set_pos(idx, pos)
 
 @socketio.on('add_frame')
 def add_frame(seq):
+  if pibo.onoff:
+    pibo.add_frame(seq)
 
 @socketio.on('remove_frame')
 def remove_frame(seq):
+  if pibo.onoff:
+    pibo.remove_frame(seq)
 
 @socketio.on('init_frame')
 def init_frame():
+  if pibo.onoff:
+    pibo.init_frame()
 
 @socketio.on('play_frame')
-def set_motion(cycle):
+def play_frame(cycle):
+  if pibo.onoff:
+    pibo.play_frame(cycle)
 
 @socketio.on('add_motion')
 def add_motion(name):
+  if pibo.onoff:
+    pibo.add_motion(name)
 
 @socketio.on('load_motion')
 def load_motion(name):
+  if pibo.onoff:
+    pibo.load_motion(name)
 
 @socketio.on('del_motion')
 def del_motion(name):
+  if pibo.onoff:
+    pibo.del_motion(name)
 
 @socketio.on('save')
-def export():
+def save():
+  if pibo.onoff:
+    pibo.save()
 
 @socketio.on('display')
 def display():
+  if pibo.onoff:
+    pibo.display()
 
 @socketio.on('reset')
 def reset():
+  if pibo.onoff:
+    pibo.reset()
 
-def emit(__key, __data):
-  sockeio.emit(__key, __data, callback=None)
+@socketio.on('onoff')
+def onoff(d=None):
+  if d != None:
+    if d == 'on':
+      pibo.start()
+    if d == 'off':
+      pibo.stop()
+  socketio.emit('onoff', "on" if pibo.onoff else "off")
+
+def emit(__key, __data, callback=None):
+  socketio.emit(__key, __data, callback=callback)
+
+def system_loop():
+  while True:
+    res = os.popen('/home/pi/openpibo-tools/project2/system.sh').read().split(',')
+    socketio.emit("system", res)
+    time.sleep(10)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -82,6 +129,7 @@ if __name__ == '__main__':
   import network_disp
   print("Network Display:", network_disp.run())
 
+  Thread(name="system_loop", target=system_loop, args=(), daemon=True).start()
+
   pibo = Pibo(emit)
-  pibo.start()
   socketio.run(app, host='0.0.0.0', port=args.port)
