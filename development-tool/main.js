d3c61620ef1669775abe63be45e7bb3830c3ea50
leeyunjai82 <leeyunjai@circul.us>
2022-05-24 18:01:57 +0900

    update

--- development-tool/main.js
+++ development-tool/main.js
@@ -12,14 +12,13 @@ const codeExec = {
   'nodejs': 'node',
   'shell': 'sh',
   };
-
 const port = process.argc > 2 ? Number(process.argv[2]):50000;
 
 let record = '';
-let ps = undefined;
+let ps;
 
 server.listen(port, function(){
-  execSync('v4l2-ctl -c vertical_flip=1,horizontal_flip=1,white_balance_auto_preset=3')
+  ps = spawn('v4l2-ctl', ['-c','vertical_flip=1,horizontal_flip=1,white_balance_auto_preset=3'])
   console.log('Server Start: ', port);
 });
 
@@ -29,45 +28,38 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-
   socket.on('stop', function(path){
-    if(ps != undefined)
-      spawnSync('kill', [ '-9', ps.pid]);
+    spawnSync('kill', [ '-9', ps.pid]);
   });
 
   socket.on('view', function(path){
     fs.readFile(path, function(err, data){
-      if(!err) io.emit('update', {'image':Buffer.from(data).toString('base64'), 'record':'## Image file load ... done ##'});
+      if(!err) io.emit('update', {'image':Buffer.from(data).toString('base64'), 'record':'## Load File: ' + path});
       else io.emit('update', {'record':err.toString()});
     });
   });
 
   socket.on('load', function(path){
     fs.readFile(path, function(err, data){
-      if(!err) io.emit('update', {'code': data.toString(), 'record':'## Code file load ... done ##'});
+      if(!err) io.emit('update', {'code': data.toString(), 'record':'## Load File: ' + path});
       else io.emit('update', {'code':'', 'record':err.toString()});
     });
   });
 
   socket.on('system', function(){
-    res = execSync('/home/pi/openpibo-tools/development-tool/system.sh').toString().split(',')
-    io.emit('system', res)
+    io.emit('system', execSync('/home/pi/openpibo-tools/development-tool/system.sh').toString().split(','));
   });
 
   socket.on('compile', function(d){
     const EXEC = codeExec[d['type']];
     const codepath = d['path'];
-    if(ps != undefined)
-      spawnSync('kill', [ '-9', ps.pid]);
 
+    spawnSync('kill', [ '-9', ps.pid]);
     fs.writeFileSync(codepath, d['text']);
 
     record = '[' + new Date().toString().split(' GMT')[0] + ']: $ sudo ' + EXEC + ' ' + codepath + ' >> \n\n';
-    io.emit('update', {'record':record});
 
-    // python3 / node / sh
-    ps = (EXEC == 'python3')?spawn(EXEC, ['-u', codepath]):spawn(EXEC, [codepath]);
-
+    ps = (EXEC == 'python3')?spawn(EXEC, ['-u', codepath]):spawn(EXEC, [codepath]); // python3/node/sh
     ps.stdout.on('data', function(data){
       record += data.toString();
       io.emit('update', {'record':record});
@@ -79,8 +71,7 @@ io.on('connection', function(socket){
     });
 
     ps.on('close', function(code){
-      record += '\n## All programs terminated ##'
-      io.emit('update', {'record':record});
+      // pass
     });
   });
 });
