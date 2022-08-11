68fcaa10ca8591867ab3aba7ba813d87c2421c0c
leeyunjai82 <leeyunjai@circul.us>
2022-08-11 11:57:56 +0900

    update

--- ide/static/index.js
+++ ide/static/index.js
@@ -158,6 +158,107 @@ document.getElementById("view").addEventListener("click", () => {
   }
 });
 
+document.getElementById("example1").addEventListener("click", () => {
+  save_code =
+'\
+import time\n\
+from openpibo.device import Device\n\
+\n\
+device = Device()\n\
+\n\
+device.send_cmd(Device.code_list["NEOPIXEL_EACH"], "255,0,0,255,0,0")\n\
+time.sleep(1)\n\
+device.send_cmd(Device.code_list["NEOPIXEL_EACH"], "0,255,0,0,255,0")\n\
+time.sleep(1)\n\
+device.send_cmd(Device.code_list["NEOPIXEL_EACH"], "0,0,255,0,0,255")\n\
+time.sleep(1)\n\
+\n\
+device.send_cmd(Device.code_list["NEOPIXEL_EACH"], "0,0,0,0,0,0")\n\
+'
+  editor.setValue(save_code);
+});
+
+document.getElementById("example2").addEventListener("click", () => {
+  save_code =
+'\
+import time\n\
+from openpibo.motion import Motion\n\
+\n\
+motion = Motion()\n\
+\n\
+motion.set_speeds([50,50,50,50,50, 50,50,50,50,50])\n\
+motion.set_accelerations([10,10,10,10,10,10,10,10,10,10])\n\
+\n\
+motion.set_motors([0,0, 30,0, 20,0, 0,0, 20,0])\n\
+time.sleep(2)\n\
+motion.set_motors([0,0,-30,0,-20,0, 0,0,-20,0])\n\
+time.sleep(2)\n\
+motion.set_motors([0,0,-80,0,  0,0, 0,0, 80,0])\n\
+time.sleep(2)\n\
+'
+  editor.setValue(save_code);
+});
+
+document.getElementById("example3").addEventListener("click", () => {
+  save_code =
+'\
+from openpibo.vision import Camera\n\
+from openpibo.vision import Detect\n\
+\n\
+camera = Camera()\n\
+detect = Detect()\n\
+\n\
+img = camera.read()\n\
+objs = detect.detect_object(img)\n\
+\n\
+for obj in objs:\n\
+	x1,y1,x2,y2 = obj["position"]\n\
+	colors = (100,100,200)\n\
+	camera.rectangle(img, (x1,y1), (x2, y2),colors,1)\n\
+	camera.putText(img, obj["name"], (x1-10, y1-10),0.6,colors,2)\n\
+\n\
+camera.imwrite("/home/pi/code/test.jpg", img)\n\
+print("이미지보기의 파일경로에 /home/pi/code/test.jpg를 입력하고 불러오기를 클릭하세요")\n\
+'
+  editor.setValue(save_code);
+});
+
+document.getElementById("example4").addEventListener("click", () => {
+  save_code =
+'\
+from openpibo.oled import Oled\n\
+\n\
+oled = Oled()\n\
+\n\
+x, y = 10, 10\n\
+text1 = "안녕하세요."\n\
+text2 = "Oled 예제입니다."\n\
+\n\
+oled.set_font(size=15)\n\
+oled.clear()\n\
+oled.draw_text((x, y), text1)\n\
+oled.draw_text((x, y+25), text2)\n\
+\n\
+oled.show()\n\
+'
+  editor.setValue(save_code);
+});
+
+document.getElementById("example5").addEventListener("click", () => {
+  save_code =
+'\
+from openpibo.speech import Speech\n\
+from openpibo.audio import Audio\n\
+\n\
+speech = Speech()\n\
+audio = Audio()\n\
+\n\
+speech.tts("<speak>안녕하세요. 음성합성 예제입니다.</speak>", "test.mp3")\n\
+audio.play(filename="test.mp3", volume=75, background=False)\n\
+'
+  editor.setValue(save_code);
+});
+
 document.getElementById("home_bt").addEventListener("click", () => {
   if (confirm("Tools로 이동하시겠습니까?(저장하지 않은 정보는 손실됩니다.)"))
     window.location.replace("http://" + window.location.hostname + ":80");
